3c220527da8825bf8cdace376589ce6dd6efa3a9
wangfangning <285132709@qq.com>
2016-11-17 15:22:19 +0800

    手厅5.0

--- wap5.0/js/public4.js
+++ wap5.0/js/public4.js
@@ -4,9 +4,7 @@ _self.fontSize = 100; // 榛樿瀛椾綋澶у皬
 console.log('ducomentBody--->'+document.body);
 //console.log('ducomentBodyclientWidth--->'+document.body.clientWidth);
 var windowWidth = document.body&&document.body.clientWidth||document.getElementsByTagName("html")[0].offsetWidth;
-if(windowWidth > 768){
-	windowWidth = 768;
-}
+
 console.log('ducomentBodyclientWidthoffsetWidth--->'+document.getElementsByTagName("html")[0].offsetWidth);
 _self.widthProportion =  function (){ var p = (windowWidth)/_self.width;return p>1?1:p<0.2?0.2:p;};
 _self.changePage =  function (){
